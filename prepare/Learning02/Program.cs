025a795f7ce3b6816cbe76fe83ce3dffc202571c
gmaxstores <maxwellnvani@gmail.com>
2024-01-17 22:05:24 +0100

    i finished the learning 2 activity as required

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Fintech";
+        job1._startYear = 2018;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Web Designer";
+        job2._company = "Fornix";
+        job2._startYear = 2010;
+        job2._endYear = 2017;
+        
+
+        Resume myResume = new Resume();
+        myResume._name = "Patrick David";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.DisplayJobs();
     }
 }
