7da5f3f0a5f58167b15dc49a99392f05f8db6596
gmaxstores <maxwellnvani@gmail.com>
2024-01-20 14:15:41 +0100

    i finished learning activity 3

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction newFraction1 = new Fraction();
+        Console.WriteLine(newFraction1.GetFractionString());
+        Console.WriteLine(newFraction1.GetFractionDecimal());
+
+        Fraction newFraction2 = new Fraction(5);
+        Console.WriteLine(newFraction2.GetFractionString());
+        Console.WriteLine(newFraction2.GetFractionDecimal());
+
+        Fraction newFraction3 = new Fraction(3, 4);
+        Console.WriteLine(newFraction3.GetFractionString());
+        Console.WriteLine(newFraction3.GetFractionDecimal());
+
+        Fraction newFraction = new Fraction(1, 3);
+        Console.WriteLine(newFraction.GetFractionString());
+        Console.WriteLine(newFraction.GetFractionDecimal());
     }
 }
