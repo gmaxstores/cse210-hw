2740c72077ac347ec0ff28fba64dfc3b954a2522
gmaxstores <maxwellnvani@gmail.com>
2024-02-03 12:29:27 +0100

    redid the week 4 dev assignment by adding a comment to exceed requirement in program.cs

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,12 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        //ListingActivity listingActivity = new ListingActivity("Listing Activity", "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.");
-        //listingActivity.Run();
-        //BreathingActivity breathingActivity = new BreathingActivity("Breathing Activity", "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.");
-        //breathingActivity.Run();
-        //ReflectingActivity reflectingActivity = new ReflectingActivity("Reflection Activity", "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.");
-        //reflectingActivity.Run();
+        //I have kept track of the number of all activities done and also computed and displayed the total number of activities to exceed requirement
         Menu menu = new Menu();
         menu.DisplayMenu();
     }
